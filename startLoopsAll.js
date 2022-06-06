33f7514b9c106de64efae6f93d0c1057d282e786
generilisk <generilisk@users.noreply.github.com>
2022-06-06 09:30:55 -0700

    Standardize naming
    Renamed as below:
    "loopingSecCheck.js"->"loopWeaken.js"
    "loopingGrow.js"->"loopGrow.js"
    "loopingHack.js"->"loopHack.js"

--- startLoopsAll.js
+++ startLoopsAll.js
@@ -4,11 +4,11 @@ export async function main(ns) {
 	ns.disableLog("ALL")
 	ns.print("Hello there")
 	let targetList = rootAccessList(ns)
-	let homeThreadCount = Math.floor((ns.getServerMaxRam("home") / (ns.getScriptRam("loopingSecCheck.js", "home") + ns.getScriptRam("loopingGrow.js", "home") + ns.getScriptRam("loopingHack.js", "home"))) / targetList.length)
+	let homeThreadCount = Math.floor((ns.getServerMaxRam("home") / (ns.getScriptRam("loopWeaken.js", "home") + ns.getScriptRam("loopGrow.js", "home") + ns.getScriptRam("loopHack.js", "home"))) / targetList.length)
 	let targetThreadCount = 0
 	let targetRAM = 0
 	let targetCost = 0
-	let hackLoops = ["loopingSecCheck.js","loopingGrow.js","loopingHack.js"]
+	let hackLoops = ["loopWeaken.js","loopGrow.js","loopHack.js"]
 	ns.print("Hacking " + targetList + " with " + homeThreadCount + " threads each")
 	for (let targetServer of targetList) {
 
@@ -17,18 +17,18 @@ export async function main(ns) {
 
 		ns.print(targetServer + " is current target. It has " + targetRAM + "GB of RAM.")
 		ns.print(targetServer + " gets " + homeThreadCount + " threads on home.")
-		ns.exec("loopingSecCheck.js", "home", homeThreadCount, targetServer)
-		ns.exec("loopingGrow.js", "home", homeThreadCount, targetServer)
-		ns.exec("loopingHack.js", "home", homeThreadCount, targetServer)
-		targetCost = (ns.getScriptRam("loopingSecCheck.js", targetServer) + ns.getScriptRam("loopingGrow.js", targetServer) + ns.getScriptRam("loopingHack.js", targetServer))
+		ns.exec("loopWeaken.js", "home", homeThreadCount, targetServer)
+		ns.exec("loopGrow.js", "home", homeThreadCount, targetServer)
+		ns.exec("loopHack.js", "home", homeThreadCount, targetServer)
+		targetCost = (ns.getScriptRam("loopWeaken.js", targetServer) + ns.getScriptRam("loopGrow.js", targetServer) + ns.getScriptRam("loopHack.js", targetServer))
 		ns.print("Each thread takes " + targetCost + "GB of RAM.")
 		if (targetRAM > 0) {
 			targetThreadCount = Math.floor(targetRAM / targetCost)
 			ns.killall(targetServer,true)
 			if (targetThreadCount < 1) { targetThreadCount = 1 };
-			ns.exec("loopingSecCheck.js", targetServer, targetThreadCount, targetServer)
-			ns.exec("loopingGrow.js", targetServer, targetThreadCount, targetServer)
-			ns.exec("loopingHack.js", targetServer, targetThreadCount, targetServer)
+			ns.exec("loopWeaken.js", targetServer, targetThreadCount, targetServer)
+			ns.exec("loopGrow.js", targetServer, targetThreadCount, targetServer)
+			ns.exec("loopHack.js", targetServer, targetThreadCount, targetServer)
 		}
 	}
 }
