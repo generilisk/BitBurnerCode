0a51977e1d267a50c81b98937d829e54776be7fd
generilisk <generilisk@users.noreply.github.com>
2022-06-08 15:56:10 -0700

    Fixed edge cases with rounding.
    Occasionally the rounding would not leave enough RAM for the final loopHack to run.
    
    Changed the if statement at the end to keep servers with RAM less than a full loop*.js from running anything. No more n00dles running loopWeaken and loopGrow without loopHack.

--- startLoopsAll.js
+++ startLoopsAll.js
@@ -4,19 +4,29 @@ export async function main(ns) {
 	ns.disableLog("ALL")
 	ns.print("Hello there")
 	let targetList = rootAccessList(ns)
-	let homeThreadCount = Math.floor((ns.getServerMaxRam("home") / (ns.getScriptRam("loopWeaken.js", "home") + ns.getScriptRam("loopGrow.js", "home") + ns.getScriptRam("loopHack.js", "home"))) / targetList.length)
+	let homeRAM = ns.getServerMaxRam("home")
+	let homeWeakenRAM = ns.getScriptRam("loopWeaken.js", "home")
+	let homeGrowRAM = ns.getScriptRam("loopGrow.js", "home")
+	let homeHackRAM = ns.getScriptRam("loopHack.js", "home")
+	let homeLoopTotalRAM = homeWeakenRAM + homeGrowRAM + homeHackRAM
+	ns.print("Weaken RAM: " + homeWeakenRAM)
+	ns.print("Grow RAM: " + homeGrowRAM)
+	ns.print("Hack RAM: " + homeHackRAM)
+	ns.print("Home RAM is " + homeRAM + ", a full set of loops uses " + homeLoopTotalRAM + " of RAM, split across " + targetList.length + " servers being hacked.")
+	let homeThreadCount = Math.floor((homeRAM / homeLoopTotalRAM) / targetList.length) - 1
 	let targetThreadCount = 0
 	let targetRAM = 0
 	let targetCost = 0
-	let hackLoops = ["loopWeaken.js","loopGrow.js","loopHack.js"]
+	let hackLoops = ["loopWeaken.js", "loopGrow.js", "loopHack.js"]
 	ns.print("Hacking " + targetList + " with " + homeThreadCount + " threads each")
 	for (let targetServer of targetList) {
 
 		targetRAM = ns.getServerMaxRam(targetServer)
-		await ns.scp(hackLoops,"home",targetServer)
+		await ns.scp(hackLoops, "home", targetServer)
 
 		ns.print(targetServer + " is current target. It has " + targetRAM + "GB of RAM.")
 		ns.print(targetServer + " gets " + homeThreadCount + " threads on home.")
+
 		ns.exec("loopWeaken.js", "home", homeThreadCount, targetServer)
 		ns.exec("loopGrow.js", "home", homeThreadCount, targetServer)
 		ns.exec("loopHack.js", "home", homeThreadCount, targetServer)
@@ -24,11 +34,12 @@ export async function main(ns) {
 		ns.print("Each thread takes " + targetCost + "GB of RAM.")
 		if (targetRAM > 0) {
 			targetThreadCount = Math.floor(targetRAM / targetCost)
-			ns.killall(targetServer,true)
-			if (targetThreadCount < 1) { targetThreadCount = 1 };
-			ns.exec("loopWeaken.js", targetServer, targetThreadCount, targetServer)
-			ns.exec("loopGrow.js", targetServer, targetThreadCount, targetServer)
-			ns.exec("loopHack.js", targetServer, targetThreadCount, targetServer)
+			ns.killall(targetServer, true)
+			if (targetThreadCount > 0) {
+				ns.exec("loopWeaken.js", targetServer, targetThreadCount, targetServer)
+				ns.exec("loopGrow.js", targetServer, targetThreadCount, targetServer)
+				ns.exec("loopHack.js", targetServer, targetThreadCount, targetServer)
+			};
 		}
 	}
 }
