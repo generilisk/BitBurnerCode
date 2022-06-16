a29b968c439e9116ffc3ba006862413df2f48cb1
Will Hughes <generilisk@gmail.com>
2022-06-16 13:56:21 -0700

    Create dayOne.js

---
+++ dayOne.js
@@ -0,0 +1,15 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	const costToRun = ns.getScriptRam("BitBurnerHacknetMgr.js") + ns.getScriptRam("QTest.js")
+	let serverRAM = 0
+	let serverName = "dayOne"
+	if (!ns.getPurchasedServers().includes(serverName)) {
+		for (let power = 1; serverRAM < costToRun; power++) {
+			serverRAM = 2 ** power
+		}
+		ns.purchaseServer(serverName, serverRAM)
+		ns.tprintf("Purchased dayOne with " + serverRAM + "GB of RAM for $" + ns.getPurchasedServerCost(serverRAM))
+		await ns.scp("BitBurnerHacknetMgr.js", "home", serverName)
+		await ns.scp("QTest.js", "home", serverName)
+	}
+}
