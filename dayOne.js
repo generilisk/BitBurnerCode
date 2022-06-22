adaabef937a9b8fdaf8a89e9e556c07e97f3876e
generilisk <generilisk@users.noreply.github.com>
2022-06-22 09:00:03 -0700

    dayOne Cleanup

--- dayOne.js
+++ dayOne.js
@@ -3,6 +3,7 @@ export async function main(ns) {
 	const costToRun = ns.getScriptRam("BitBurnerHacknetMgr.js") + ns.getScriptRam("QTest.js")
 	let serverRAM = 0
 	let serverName = "dayOne"
+	ns.exec("infiltrate.js",ns.getHostname())
 	if (!ns.getPurchasedServers().includes(serverName)) {
 		for (let power = 1; serverRAM < costToRun; power++) {
 			serverRAM = 2 ** power
@@ -11,10 +12,18 @@ export async function main(ns) {
 		ns.tprintf("Purchased dayOne with " + serverRAM + "GB of RAM for $" + ns.getPurchasedServerCost(serverRAM))
 		await ns.scp("BitBurnerHacknetMgr.js", "home", serverName)
 		await ns.scp("QTest.js", "home", serverName)
+		await ns.scp("tools.js", "home", serverName)
+		await ns.scp("hackThePlanet.js", "home", serverName)
 	}
 	if (ns.getPurchasedServers().includes(serverName)){
 		if(!ns.isRunning("BitBurnerHacknetMgr.js",serverName)){
 			ns.exec("BitBurnerHacknetMgr.js",serverName)
-		}		
+		}
+		if(!ns.isRunning("QTest.js",serverName)){
+			ns.exec("QTest.js",serverName)
+		}
+		if(!ns.isRunning("hackThePlanet.js",serverName)){
+			ns.exec("hackThePlanet.js",serverName)
+		}
 	}
 }
